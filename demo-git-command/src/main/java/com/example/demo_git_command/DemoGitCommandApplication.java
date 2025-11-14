238a56bef0a4372f03bdd3131c743eb9ad40e87e
RamaGopal-ZapCom <rama.gopal@zapcg.com>
2025-11-14 14:45:06 +0530

    added files

---
+++ demo-git-command/src/main/java/com/example/demo_git_command/DemoGitCommandApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_git_command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitCommandApplication.class, args);
+	}
+
+}
